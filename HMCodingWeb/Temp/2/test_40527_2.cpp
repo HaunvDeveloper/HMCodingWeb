bb4793118daceacf1919193aa338ef5f3374d12d
HaunvDeveloper <haunv.cntt@gmail.com>
2025-06-26 01:41:08 +0700

    Update Cache

---
+++ HMCodingWeb/Temp/2/test_40527_2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    long long a, b;
+    cin>>a>>b;
+    long long kq;
+    kq=pow(a,2)+2*a*b+pow(b,2);
+    cout<<kq;
+    return 0;
+}
